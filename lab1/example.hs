2443207d8a95e3779ccbac42eed1c57a502a69e9
Patryk Papiór <papipatr@borg.kis.agh.edu.pl>
2018-04-26 12:04:09 +0200

    factorial and fib sequence

--- lab1/example.hs
+++ lab1/example.hs
@@ -5,3 +5,14 @@ specialCases 2 = " "
 specialCases 3 = "World"
 specialCases 4 = "!"
 specialCases x = "???"
+
+
+-- okreslanie typow przyjmowanych przez funkcje i zwracanych
+silnia::Int -> Int
+silnia 0 = 1
+silnia n = n * silnia (n-1)
+
+fib::Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
---
+++ lab1/silnia.hs
@@ -0,0 +1,4 @@
+silnia::Int->Int
+
+silnia 0 = 1
+silnia n = n * silnia (n-1)
